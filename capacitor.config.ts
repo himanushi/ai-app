ce3997fea88d71c819c616651d8064447ce12867
himanushi <himanushi@users.noreply.github.com>
2023-02-24 09:29:56 +0900

    add ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'net.himakan.app.aiimage',
+	appName: 'AI画像生成',
+	bundledWebRuntime: false,
+	webDir: 'build'
+};
+
+export default config;
