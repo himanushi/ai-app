9f5e56b367a51bf2cd9bea19db10cb85442ccf4d
himanushi <himanushi@users.noreply.github.com>
2023-02-28 08:07:52 +0900

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,35 @@
 <script lang="ts">
 	import { StableDiffusion } from 'capacitor-plugin-stable-diffusion';
-	import { Filesystem, Directory, type ReaddirResult } from '@capacitor/filesystem';
+	import { Filesystem, Directory } from '@capacitor/filesystem';
+	import { onMount } from 'svelte';
+
+	let dProgress = 0;
+	let dStatus = '';
+	let dError: string | undefined = '';
+	let zStatus = '';
+	let gProgress = 0;
+	let gStatus = '';
+	let gError: string | undefined = '';
+
+	onMount(() => {
+		StableDiffusion.addListener('downloadProgress', (data) => {
+			dProgress = data.progress;
+		});
+		StableDiffusion.addListener('downloadDidComplete', (data) => {
+			dStatus = data.state;
+			dError = data.error;
+		});
+		StableDiffusion.addListener('unzipDidComplete', (data) => {
+			zStatus = data.state;
+		});
+		StableDiffusion.addListener('generateProgress', (data) => {
+			gProgress = data.progress;
+		});
+		StableDiffusion.addListener('generateDidComplete', (data) => {
+			gStatus = data.state;
+			gError = data.error;
+		});
+	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -15,35 +44,42 @@
 			url: 'https://huggingface.co/coreml/coreml-stable-diffusion-2-1-base/resolve/main/split_einsum/stable-diffusion-v2.1-base_no-i2i_split-einsum.zip',
 			modelsDirName: 'models'
 		})}>download</button
->
+><br />
+dProgress: {dProgress}<br />
+dStatus: {dStatus}<br />
+dError: {dError}<br /><br />
 <button
 	on:click={() =>
 		StableDiffusion.unzip({
 			url: 'https://huggingface.co/coreml/coreml-stable-diffusion-2-1-base/resolve/main/split_einsum/stable-diffusion-v2.1-base_no-i2i_split-einsum.zip',
 			modelsDirName: 'models'
 		})}>unzip</button
->
+><br />
+zStatus: {zStatus}<br /><br />
 <button
 	on:click={() =>
 		StableDiffusion.generateTextToImage({
 			modelPath: 'models/stable-diffusion-v2.1-base_no-i2i_split-einsum',
 			prompt: 'a photo of car'
 		})}>generateTextToImage</button
->
+><br />
+gProgress: {gProgress}<br />
+gStatus: {gStatus}<br />
+gError: {gError}<br /><br />
 <button
 	on:click={async () =>
 		await Filesystem.readdir({
 			path: '/',
 			directory: Directory.Documents
 		})}>Get Files</button
->
+><br /><br />
 <button
 	on:click={async () =>
 		await Filesystem.readdir({
 			path: '/models',
 			directory: Directory.Documents
 		})}>Get Models Files</button
->
+><br /><br />
 <button
 	on:click={async () =>
 		await Filesystem.mkdir({
