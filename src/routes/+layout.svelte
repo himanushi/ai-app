fa4be269f5a8e8b456cf3e38bedd0dc93e1c9498
himanushi <himanushi@users.noreply.github.com>
2023-02-28 23:27:17 +0900

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 	import '~/theme/custom.css';
 	import { initialize } from '@ionic/core';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import Icon from '~/components/icon.svelte';
+
 	onMount(() => {
 		initialize({
 			animated: true,
@@ -12,12 +15,24 @@
 </script>
 
 <ion-app>
-	<ion-header>
-		<ion-toolbar>
-			<ion-title> AI画像生成 </ion-title>
-		</ion-toolbar>
-	</ion-header>
+	<ion-header><ion-toolbar /></ion-header>
 	<ion-content force-overscroll={false}>
-		<slot />
+		<ion-tabs>
+			<ion-tab tab="generate"><slot /></ion-tab>
+			<ion-tab tab="images"><slot /> </ion-tab>
+			<ion-tab tab="file"><slot /></ion-tab>
+			<ion-tab-bar slot="bottom">
+				<ion-tab-button tab="generate" on:click={() => goto('/generate')}>
+					<Icon name="brush" size="l" fill />
+				</ion-tab-button>
+				<ion-tab-button tab="images" on:click={() => goto('/images')}>
+					<Icon name="photo_library" size="l" fill />
+				</ion-tab-button>
+				<ion-tab-button tab="file" on:click={() => goto('/file')}>
+					<Icon name="folder" size="l" fill />
+				</ion-tab-button>
+			</ion-tab-bar>
+		</ion-tabs>
 	</ion-content>
+	<ion-footer><ion-toolbar /></ion-footer>
 </ion-app>
