9c2eee7a70d9f9bd43cfb09c1de8574fc0f54b5c
himanushi <himanushi@users.noreply.github.com>
2023-02-28 20:38:26 +0900

    fix

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '~/theme/variables.css';
+	import '~/theme/custom.css';
+	import { initialize } from '@ionic/core';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		initialize({
+			animated: true,
+			mode: 'ios'
+		});
+	});
+</script>
+
+<ion-app>
+	<ion-header>
+		<ion-toolbar>
+			<ion-title> AI画像生成 </ion-title>
+		</ion-toolbar>
+	</ion-header>
+	<ion-content>
+		<slot />
+	</ion-content>
+</ion-app>
