6870b499575eb6fcc873fcf74965e0ca98fbab44
himanushi <himanushi@users.noreply.github.com>
2023-03-02 07:42:54 +0900

    fix

---
+++ src/routes/generate/prompt.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let prompt: string | null | undefined;
+
+	const words: { weight: number; word: string }[] = [];
+</script>
+
+<ion-item-divider>
+	<ion-label> Prompt </ion-label>
+</ion-item-divider>
+<ion-item>
+	<ion-textarea
+		auto-grow={true}
+		value={prompt}
+		on:ionChange={(ion) => (prompt = ion.detail.value)}
+	/>
+</ion-item>
