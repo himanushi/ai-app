9c24c8ed8f6d034786ed24f8045e731ce23566a5
himanushi <himanushi@users.noreply.github.com>
2023-03-02 07:42:19 +0900

    fix

--- src/routes/file/[...path]/+page.svelte
+++ src/routes/file/[...path]/+page.svelte
@@ -13,39 +13,57 @@
 		.slice(0, -1)
 		.join('/');
 	const href = (fileName: string) => ['/file', path, fileName].filter((pt) => pt !== '').join('/');
+	const filePath = (fileName: string) => [path, fileName].filter((pt) => pt !== '').join('/');
+	const requiredFiles = ['images', 'models'];
 	let dir: ReaddirResult;
+
+	const deleteFile = async (fileName: string) => {
+		await Filesystem.deleteFile({
+			directory: Directory.Documents,
+			path: filePath(fileName)
+		});
+		dir = await Filesystem.readdir({
+			directory: Directory.Documents,
+			path
+		});
+	};
 
 	$: (async () => {
 		dir = await Filesystem.readdir({
-			path,
-			directory: Directory.Documents
+			directory: Directory.Documents,
+			path
 		});
 	})();
 </script>
 
-<ion-item-group>
+<ion-list>
 	{#if dir}
 		{#each dir.files as file}
 			{@const isImage = file.name.endsWith('.png')}
 			{@const isDirectory = file.type === 'directory'}
-			<ion-item href={isDirectory ? href(file.name) : undefined}>
-				{#if isImage}
-					<ion-thumbnail>
-						<ion-img src={Capacitor.convertFileSrc(file.uri)} />
-					</ion-thumbnail>
-				{:else if isDirectory}
-					<Icon fill name="folder" color="yellow" />
-				{:else}
-					<Icon fill name="description" color="blue" />
-				{/if}
-				<ion-label>
+			<ion-item-sliding>
+				<ion-item href={isDirectory ? href(file.name) : undefined}>
+					{#if isImage}
+						<ion-thumbnail>
+							<ion-img src={Capacitor.convertFileSrc(file.uri)} />
+						</ion-thumbnail>
+					{:else if isDirectory}
+						<Icon fill name="folder" color="yellow" />
+					{:else}
+						<Icon fill name="description" color="blue" />
+					{/if}
 					{file.name}
-				</ion-label>
-			</ion-item>
+				</ion-item>
+				<ion-item-options side="end">
+					<ion-item-option color="danger" on:click={() => deleteFile(file.name)}>
+						<Icon name="delete" fill />
+					</ion-item-option>
+				</ion-item-options>
+			</ion-item-sliding>
 		{/each}
 	{/if}
 	<ion-item href={prevHref} detail={false}>
 		<Icon fill name="chevron_left" color="gray" />
 		1つ戻る
 	</ion-item>
-</ion-item-group>
+</ion-list>
